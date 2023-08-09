1e360bc1319ee86a2bf1c4be7ccb9bf3e8a927cc
Pavol Tuka <pavol@bratia.sk>
2023-08-09 09:22:33 +0200

    Use block whitelist

--- theme/app/filters.php
+++ theme/app/filters.php
@@ -3,3 +3,15 @@
 declare(strict_types=1);
 
 namespace App;
+
+add_filter('allowed_block_types_all', fn(): array => [
+    'core/column',
+    'core/columns',
+    'core/cover',
+    'core/heading',
+    'core/image',
+    'core/list',
+    'core/paragraph',
+    'core/separator',
+    'theme/latest-default-category-posts',
+], 10, 2);
--- theme/resources/scripts/blocks/latest-default-category-posts.block.jsx
+++ theme/resources/scripts/blocks/latest-default-category-posts.block.jsx
@@ -2,7 +2,9 @@ import ServerSideRender from '@wordpress/server-side-render';
 
 export default {
     name: 'theme/latest-default-category-posts',
-    title: 'Najnovšie články z predvolenej kategórie',
+    title: 'Najnovšie články',
+    description: 'Zobrazte najnovšie články z predvolenej kategórie. Ak ju chcete zmeniť prejdite do Nastavenia -> Písanie.',
     category: 'theme',
+    example: {},
     edit: (props) => <ServerSideRender block={'theme/latest-default-category-posts'} attributes={props.attributes}/>
 };
