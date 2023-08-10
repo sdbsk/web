378a257c861da5fcad87443ba6984f524fd75435
Pavol Tuka <pavol@bratia.sk>
2023-08-10 15:17:50 +0200

    Add template to call-to-action.block.jsx

--- theme/resources/scripts/blocks/call-to-action.block.jsx
+++ theme/resources/scripts/blocks/call-to-action.block.jsx
@@ -1,20 +1,20 @@
 import {InnerBlocks, useBlockProps} from '@wordpress/block-editor';
 
+const TEMPLATE = [
+    ['core/heading', {content: 'Nadpis výzvy k akcii', level: 2}],
+    ['core/paragraph', {content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque at justo viverra metus egestas hendrerit. In sed mauris eget mi vestibulum interdum sit amet ut magna. Sed eu molestie mi. Cras malesuada ut neque vel mollis. Pellentesque sollicitudin quis sapien eu tincidunt. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Fusce metus quam, elementum vitae accumsan tristique, consequat non turpis.'}],
+    ['core/button', {text: 'Vestibulum ante ipsum'}]
+];
+
 export default {
     name: 'theme/call-to-action',
     title: 'Výzva k akcii',
     description: 'Vytvorte pútavý blok s nadpisom, popisom a tlačidlom.',
     category: 'theme',
-    example: {
-        innerBlocks: [
-            {name: 'core/heading', attributes: {content: 'Nadpis', level: 2}},
-            {name: 'core/paragraph', attributes: {content: 'Popis'}},
-            {name: 'core/button', attributes: {text: 'Tlačidlo'}}
-        ]
-    },
+    example: {innerBlocks: TEMPLATE.map((i) => ({name: i[0], attributes: i[1]}))},
     edit: () => (
         <div {...useBlockProps()}>
-            <InnerBlocks allowedBlocks={['core/button', 'core/heading', 'core/paragraph']}/>
+            <InnerBlocks allowedBlocks={['core/button', 'core/heading', 'core/paragraph']} template={TEMPLATE} templateLock={'all'}/>
         </div>
     ),
     save: () => (
