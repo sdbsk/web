fec60fccb611a835605443fabc2f3dc3a28b374a
Pavol Tuka <pavol@bratia.sk>
2023-08-25 09:31:02 +0200

    Render visible h1 on every page

--- theme/resources/views/page.blade.php
+++ theme/resources/views/page.blade.php
@@ -1,6 +1,6 @@
 @extends('layout')
 @section('content')
-    <h1{!! is_front_page() ? ' class="d-none"' : '' !!}>{{ the_title() }}</h1>
+    <h1>{{ the_title() }}</h1>
     @while(have_posts())
         @php the_post(); the_content(); @endphp
     @endwhile
