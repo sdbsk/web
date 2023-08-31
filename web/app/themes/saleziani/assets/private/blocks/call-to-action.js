60400f80e3ed3861d70312375b96cb2a3a5e6dc4
Pavol Tuka <pavol@bratia.sk>
2023-08-31 14:11:01 +0200

    Add call-to-action block

---
+++ web/app/themes/saleziani/assets/private/blocks/call-to-action.js
@@ -0,0 +1,47 @@
+(wp => {
+    const template = [
+        ['core/heading', {content: 'Nadpis výzvy k akcii', level: 2}],
+        ['core/paragraph', {content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque at justo viverra metus egestas hendrerit. In sed mauris eget mi vestibulum interdum sit amet ut magna. Sed eu molestie mi. Cras malesuada ut neque vel mollis. Pellentesque sollicitudin quis sapien eu tincidunt. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Fusce metus quam, elementum vitae accumsan tristique, consequat non turpis.'}],
+        ['core/button', {text: 'Vestibulum ante ipsum'}]
+    ];
+
+    return wp.blocks.registerBlockType('saleziani/call-to-action', {
+        category: 'theme',
+        description: 'Vytvorte pútavý blok s nadpisom, popisom a tlačidlom.',
+        example: {innerBlocks: template.map((i) => ({name: i[0], attributes: i[1]}))},
+        icon: 'superhero-alt',
+        supports: {
+            align: true,
+            color: {
+                background: true,
+                text: false
+            },
+            customClassName: false
+        },
+        title: 'Výzva k akcii',
+        edit: props => wp.element.createElement(
+            'div',
+            wp.blockEditor.useBlockProps({className: props.className}),
+            wp.element.createElement(
+                'div',
+                {className: 'inner'},
+                wp.element.createElement(
+                    wp.blockEditor.InnerBlocks,
+                    {
+                        allowedBlocks: ['core/button', 'core/heading', 'core/paragraph'],
+                        template: template
+                    }
+                )
+            )
+        ),
+        save: () => wp.element.createElement(
+            'div',
+            wp.blockEditor.useBlockProps.save(),
+            wp.element.createElement(
+                'div',
+                {className: 'inner'},
+                wp.element.createElement(wp.blockEditor.InnerBlocks.Content)
+            )
+        )
+    });
+})(window.wp);
--- web/app/themes/saleziani/functions.php
+++ web/app/themes/saleziani/functions.php
@@ -4,10 +4,94 @@ declare(strict_types=1);
 
 $manifest = json_decode(file_get_contents(__DIR__ . '/assets/public/manifest.json'), true);
 
-add_action('admin_enqueue_scripts', function () use ($manifest): void {
+add_action('init', function (): void {
+    register_post_type('campaign', [
+        'labels' => [
+            'add_new' => __('Pridať novú', 'sage'),
+            'add_new_item' => __('Pridať novú kampaň', 'sage'),
+            'all_items' => __('Všetky kampane', 'sage'),
+            'archives' => __('Archív kampaní', 'sage'),
+            'attributes' => __('Atribúty kampaní', 'sage'),
+            'edit_item' => __('Upraviť kampaň', 'sage'),
+            'filter_items_list' => __('Filtrovať zoznam kampaní', 'sage'),
+            'insert_into_item' => __('Vložiť ku kampani', 'sage'),
+            'item_link' => __('Odkaz na kampaň', 'sage'),
+            'item_link_description' => __('Odkaz na kampaň.', 'sage'),
+            'item_published' => __('Kampaň zverejnená.', 'sage'),
+            'item_published_privately' => __('Kampaň zverejnená ako súkromný.', 'sage'),
+            'item_reverted_to_draft' => __('Kampaň vrátená do stavu koncept.', 'sage'),
+            'item_scheduled' => __('Zverejnenie kampane bolo naplánované.', 'sage'),
+            'item_updated' => __('Kampaň aktualizovaná.', 'sage'),
+            'items_list' => __('Zoznam kampaní', 'sage'),
+            'items_list_navigation' => __('Navigácia v zozname kampaní', 'sage'),
+            'name' => __('Kampane', 'sage'),
+            'new_item' => __('Nová kampaň', 'sage'),
+            'not_found' => __('Kampaň sa nenašla', 'sage'),
+            'not_found_in_trash' => __('Kampaň sa v koši nenašla', 'sage'),
+            'search_items' => __('Vyhľadať kampaň', 'sage'),
+            'singular_name' => __('Kampaň', 'sage'),
+            'uploaded_to_this_item' => __('Nahrať ku kampani', 'sage'),
+            'view_item' => __('Zobraziť stránku kampane', 'sage'),
+            'view_items' => __('Zobraziť kampane', 'sage'),
+        ],
+        'public' => true,
+        'menu_icon' => 'dashicons-megaphone',
+        'menu_position' => 20,
+        'rewrite' => ['slug' => 'kampane'],
+        'show_in_rest' => true,
+    ]);
+});
+
+add_action('enqueue_block_editor_assets', function () use ($manifest): void {
     wp_enqueue_style('editor', home_url() . $manifest['app/themes/saleziani/assets/public/editor.css']);
+
+    foreach (get_cumstom_block_types() as $blockType) {
+        wp_enqueue_script($blockType . '-block', get_template_directory_uri() . '/assets/private/blocks/' . $blockType . '.js', ['wp-blocks', 'wp-element']);
+    }
 });
 
 add_action('wp_enqueue_scripts', function () use ($manifest): void {
     wp_enqueue_style('style', home_url() . $manifest['app/themes/saleziani/assets/public/style.css']);
 });
+
+add_filter('allowed_block_types_all', function (): array {
+    $blockTypes = [
+        'core/button',
+        'core/buttons',
+        'core/cover',
+        'core/heading',
+        'core/image',
+        'core/list',
+        'core/paragraph',
+        'core/separator',
+        'wp-bootstrap-blocks/column',
+        'wp-bootstrap-blocks/container',
+        'wp-bootstrap-blocks/row',
+    ];
+
+    foreach (get_cumstom_block_types() as $blockType) {
+        $blockTypes[] = 'saleziani/' . $blockType;
+    }
+
+    return $blockTypes;
+}, 10, 2);
+
+add_filter('upload_mimes', function (array $mimes): array {
+    $mimes['svg'] = 'image/svg+xml';
+    return $mimes;
+});
+
+function get_cumstom_block_types(): array
+{
+    $blockTypes = [];
+
+    foreach (scandir(TEMPLATEPATH . '/assets/private/blocks/') as $filename) {
+        preg_match('~([a-zA-Z0-9-]+)\.js~', $filename, $matches);
+
+        if (isset($matches[1])) {
+            $blockTypes[] = $matches[1];
+        }
+    }
+
+    return $blockTypes;
+}
