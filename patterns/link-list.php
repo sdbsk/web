42e387c6674416f9886a68136e3ba0bfed336cd7
Pavol Tuka <pavol@bratia.sk>
2023-09-07 10:06:05 +0200

    Add link-list pattern

---
+++ patterns/link-list.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * Title: Zoznam odkazov
+ * Slug: saleziani/link-list
+ * Categories: saleziani
+ */
+?>
+
+<!-- wp:group {"className":"wp-pattern-saleziani-link-list"} -->
+<div class="wp-block-group wp-pattern-saleziani-link-list">
+    <!-- wp:wp-bootstrap-blocks/row {"template":"custom"} -->
+    <!-- wp:wp-bootstrap-blocks/column {"sizeSm":6} -->
+    <!-- wp:paragraph -->
+    <p>
+        <a href="#">Lorem ipsum</a>
+    </p>
+    <!-- /wp:paragraph -->
+    <!-- /wp:wp-bootstrap-blocks/column -->
+    <!-- wp:wp-bootstrap-blocks/column {"sizeSm":6} -->
+    <!-- wp:paragraph -->
+    <p>
+        <a href="#">Dolor sit amet</a>
+    </p>
+    <!-- /wp:paragraph -->
+    <!-- /wp:wp-bootstrap-blocks/column -->
+    <!-- wp:wp-bootstrap-blocks/column {"sizeSm":6} -->
+    <!-- wp:paragraph -->
+    <p>
+        <a href="#">Consectetur adipiscing elit</a>
+    </p>
+    <!-- /wp:paragraph -->
+    <!-- /wp:wp-bootstrap-blocks/column -->
+    <!-- wp:wp-bootstrap-blocks/column {"sizeSm":6} -->
+    <!-- wp:paragraph -->
+    <p>
+        <a href="#">Nam vel tristique massa</a>
+    </p>
+    <!-- /wp:paragraph -->
+    <!-- /wp:wp-bootstrap-blocks/column -->
+    <!-- /wp:wp-bootstrap-blocks/row -->
+</div>
+<!-- /wp:group -->
