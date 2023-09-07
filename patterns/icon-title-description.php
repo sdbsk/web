d4dadfe9b4a8525368cf4ed04145d9f2ee94bccf
Pavol Tuka <pavol@bratia.sk>
2023-09-07 10:39:17 +0200

    Add inner wp-block-group(s) to icon-title-description pattern

--- patterns/icon-title-description.php
+++ patterns/icon-title-description.php
@@ -13,43 +13,49 @@ declare(strict_types=1);
 <div class="wp-block-group wp-pattern-saleziani-icon-title-description">
     <!-- wp:wp-bootstrap-blocks/row {"template":"custom"} -->
     <!-- wp:wp-bootstrap-blocks/column {"sizeMd":4} -->
-    <!-- wp:image -->
-    <figure class="wp-block-image">
-        <img src="https://place-hold.it/40x40?text"/>
-    </figure>
-    <!-- /wp:image -->
-    <!-- wp:heading {"level":3,"textColor":"blackberry"} -->
-    <h3 class="wp-block-heading has-blackberry-color has-text-color">Lorem ipsum</h3>
-    <!-- /wp:heading -->
-    <!-- wp:paragraph -->
-    <p>Nam vel tristique massa, ac malesuada urna. Morbi ultrices ullamcorper leo quis maximus. Nam aliquam bibendum risus a porttitor. Etiam elit libero.</p>
-    <!-- /wp:paragraph -->
+    <!-- wp:group -->
+    <div class="wp-block-group">
+        <!-- wp:image -->
+        <figure class="wp-block-image"><img src="https://place-hold.it/40x40?text"/></figure>
+        <!-- /wp:image -->
+        <!-- wp:heading {"level":3,"textColor":"blackberry"} -->
+        <h3 class="wp-block-heading has-blackberry-color has-text-color">Lorem ipsum</h3>
+        <!-- /wp:heading -->
+        <!-- wp:paragraph -->
+        <p>Nam vel tristique massa, ac malesuada urna. Morbi ultrices ullamcorper leo quis maximus. Nam aliquam bibendum risus a porttitor. Etiam elit libero.</p>
+        <!-- /wp:paragraph -->
+    </div>
+    <!-- /wp:group -->
     <!-- /wp:wp-bootstrap-blocks/column -->
     <!-- wp:wp-bootstrap-blocks/column {"sizeMd":4} -->
-    <!-- wp:image -->
-    <figure class="wp-block-image">
-        <img src="https://place-hold.it/40x40?text"/>
-    </figure>
-    <!-- /wp:image -->
-    <!-- wp:heading {"level":3,"textColor":"blackberry"} -->
-    <h3 class="wp-block-heading has-blackberry-color has-text-color">Dolor sit amet</h3>
-    <!-- /wp:heading -->
-    <!-- wp:paragraph -->
-    <p>Integer a turpis fermentum lorem ornare dictum. Nam porta nisl eget neque auctor, non facilisis dolor placerat. Cras in posuere purus, id faucibus urna.</p>
-    <!-- /wp:paragraph -->
+    <!-- wp:group -->
+    <div class="wp-block-group">
+        <!-- wp:image -->
+        <figure class="wp-block-image"><img src="https://place-hold.it/40x40?text"/></figure>
+        <!-- /wp:image -->
+        <!-- wp:heading {"level":3,"textColor":"blackberry"} -->
+        <h3 class="wp-block-heading has-blackberry-color has-text-color">Dolor sit amet</h3>
+        <!-- /wp:heading -->
+        <!-- wp:paragraph -->
+        <p>Integer a turpis fermentum lorem ornare dictum. Nam porta nisl eget neque auctor, non facilisis dolor placerat. Cras in posuere purus, id faucibus urna.</p>
+        <!-- /wp:paragraph -->
+    </div>
+    <!-- /wp:group -->
     <!-- /wp:wp-bootstrap-blocks/column -->
     <!-- wp:wp-bootstrap-blocks/column {"sizeMd":4} -->
-    <!-- wp:image -->
-    <figure class="wp-block-image">
-        <img src="https://place-hold.it/40x40?text"/>
-    </figure>
-    <!-- /wp:image -->
-    <!-- wp:heading {"level":3,"textColor":"blackberry"} -->
-    <h3 class="wp-block-heading has-blackberry-color has-text-color">Consectetur adipiscing</h3>
-    <!-- /wp:heading -->
-    <!-- wp:paragraph -->
-    <p>In blandit mi tempor, laoreet turpis eu, volutpat ante. Suspendisse fringilla cursus semper. Ut quis iaculis nisl.</p>
-    <!-- /wp:paragraph -->
+    <!-- wp:group -->
+    <div class="wp-block-group">
+        <!-- wp:image -->
+        <figure class="wp-block-image"><img src="https://place-hold.it/40x40?text"/></figure>
+        <!-- /wp:image -->
+        <!-- wp:heading {"level":3,"textColor":"blackberry"} -->
+        <h3 class="wp-block-heading has-blackberry-color has-text-color">Consectetur adipiscing</h3>
+        <!-- /wp:heading -->
+        <!-- wp:paragraph -->
+        <p>In blandit mi tempor, laoreet turpis eu, volutpat ante. Suspendisse fringilla cursus semper. Ut quis iaculis nisl.</p>
+        <!-- /wp:paragraph -->
+    </div>
+    <!-- /wp:group -->
     <!-- /wp:wp-bootstrap-blocks/column -->
     <!-- /wp:wp-bootstrap-blocks/row -->
 </div>
