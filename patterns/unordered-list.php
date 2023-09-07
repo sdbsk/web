5192f097cd086ad1a0e48a2faad3203588e03522
Pavol Tuka <pavol@bratia.sk>
2023-09-07 13:55:50 +0200

    Add unordered-list pattern

---
+++ patterns/unordered-list.php
@@ -0,0 +1,66 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * Title: Neusporiadaný zoznam
+ * Slug: saleziani/unordered-list
+ * Categories: saleziani
+ */
+?>
+
+<!-- wp:group {"className":"wp-pattern-saleziani-unordered-list"} -->
+<div class="wp-block-group wp-pattern-saleziani-unordered-list">
+    <!-- wp:list -->
+    <ul>
+        <!-- wp:list-item -->
+        <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</li>
+        <!-- /wp:list-item -->
+    </ul>
+    <!-- /wp:list -->
+    <!-- wp:paragraph -->
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+    <!-- /wp:paragraph -->
+    <!-- wp:list -->
+    <ul>
+        <!-- wp:list-item -->
+        <li>Aliquam sit amet dictum nunc.</li>
+        <!-- /wp:list-item -->
+    </ul>
+    <!-- /wp:list -->
+    <!-- wp:paragraph -->
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+    <!-- /wp:paragraph -->
+    <!-- wp:list -->
+    <ul>
+        <!-- wp:list-item -->
+        <li>Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.
+        </li>
+        <!-- /wp:list-item -->
+    </ul>
+    <!-- /wp:list -->
+    <!-- wp:paragraph -->
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+    <!-- /wp:paragraph -->
+    <!-- wp:list -->
+    <ul>
+        <!-- wp:list-item -->
+        <li>Integer semper interdum rhoncus.</li>
+        <!-- /wp:list-item -->
+    </ul>
+    <!-- /wp:list -->
+    <!-- wp:paragraph -->
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+    <!-- /wp:paragraph -->
+    <!-- wp:list -->
+    <ul>
+        <!-- wp:list-item -->
+        <li>Donec tempus elit vel nulla eleifend, vitae tincidunt quam placerat.</li>
+        <!-- /wp:list-item -->
+    </ul>
+    <!-- /wp:list -->
+    <!-- wp:paragraph -->
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+    <!-- /wp:paragraph -->
+</div>
+<!-- /wp:group -->
