5d7dd275dbbec150ccdbea59c3ba167009ca6f2c
“Martin <martinlikavcan@gmail.com>
2023-09-08 10:29:37 +0200

    add image-title-description pattern

---
+++ patterns/image-title-description.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * Title: Obrázok, Nadpis, Popis
+ * Slug: saleziani/image-title-description
+ * Categories: saleziani
+ */
+?>
+
+<!-- wp:group {"className":"wp-pattern-saleziani-image-title-description"} -->
+<div class="wp-block-group wp-pattern-saleziani-image-title-description"><!-- wp:group {"className":"row"} -->
+    <div class="wp-block-group row"><!-- wp:group {"className":"col-12 col-sm-6"} -->
+        <div class="wp-block-group col-12 col-sm-6"><!-- wp:heading {"level":3} -->
+            <h3 class="wp-block-heading">Lorem ipsum dolor sit amet</h3>
+            <!-- /wp:heading -->
+            <!-- wp:paragraph -->
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. </p>
+            <!-- /wp:paragraph --></div>
+        <!-- /wp:group -->
+
+        <!-- wp:group {"className":"col-12 col-sm-6"} -->
+        <div class="wp-block-group col-12 col-sm-6"><!-- wp:image {"align":"full","id":22,"sizeSlug":"full","linkDestination":"none"} -->
+            <figure class="wp-block-image alignfull size-full"><img src="https://place-hold.it/370x185" alt="" class="wp-image-22"/></figure>
+            <!-- /wp:image --></div>
+        <!-- /wp:group --></div>
+    <!-- /wp:group --></div>
+<!-- /wp:group -->
