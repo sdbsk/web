617804bde7e63ac86dc36b1220ab45b7b1b4e5da
Pavol Tuka <pavol@bratia.sk>
2023-09-08 10:34:34 +0200

    Reformat scss

---
+++ patterns/title-description-image.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * Title: Nadpis, popis a obrázok
+ * Slug: saleziani/title-description-image
+ * Categories: saleziani
+ */
+?>
+
+<!-- wp:group {"className":"wp-pattern-saleziani-title-description-image"} -->
+<div class="wp-block-group wp-pattern-saleziani-title-description-image">
+    <!-- wp:group {"className":"row"} -->
+    <div class="wp-block-group row">
+        <!-- wp:group {"className":"col-12 col-sm-6"} -->
+        <div class="wp-block-group col-12 col-sm-6">
+            <!-- wp:heading {"level":3} -->
+            <h3 class="wp-block-heading">Lorem ipsum dolor sit amet</h3>
+            <!-- /wp:heading -->
+            <!-- wp:paragraph -->
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. </p>
+            <!-- /wp:paragraph -->
+        </div>
+        <!-- /wp:group -->
+        <!-- wp:group {"className":"col-12 col-sm-6"} -->
+        <div class="wp-block-group col-12 col-sm-6">
+            <!-- wp:image -->
+            <figure class="wp-block-image">
+                <img src="https://place-hold.it/370x185"/>
+            </figure>
+            <!-- /wp:image -->
+        </div>
+        <!-- /wp:group -->
+    </div>
+    <!-- /wp:group -->
+</div>
+<!-- /wp:group -->
