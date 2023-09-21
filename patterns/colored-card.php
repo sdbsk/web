c3b57dcef72c7f6b2753bea991ee1b091e33776c
“Martin <martinlikavcan@gmail.com>
2023-09-21 15:47:03 +0200

    add transparent and colored cards

---
+++ patterns/colored-card.php
@@ -0,0 +1,73 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * Title: Farebné kartičky
+ * Slug: saleziani/colored-cards
+ * Categories: saleziani
+ */
+?>
+
+<!-- wp:group {"className":"wp-pattern-saleziani-colored-cards"} -->
+<div class="wp-block-group wp-pattern-saleziani-colored-cards"><!-- wp:group {"className":"row"} -->
+    <div class="wp-block-group row"><!-- wp:group {"className":"col-12 col-sm-6"} -->
+        <div class="wp-block-group col-12 col-sm-6"><!-- wp:group {"backgroundColor":"potato","className":"card"} -->
+            <div class="wp-block-group card has-potato-background-color has-background">
+                <!-- wp:image -->
+                <figure class="wp-block-image"><img src="<?php echo placeholder_image_path(570, 356) ?>"/></figure>
+                <!-- /wp:image -->
+
+                <!-- wp:group {"className":"content-left"} -->
+                <div class="wp-block-group content-left"><!-- wp:heading {"level":3,"textColor":"white"} -->
+                    <h3 class="wp-block-heading has-white-color has-text-color">Nadpis</h3>
+                    <!-- /wp:heading -->
+
+                    <!-- wp:paragraph {"textColor":"white"} -->
+                    <p class="has-white-color has-text-color">Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                        sed do eiusmod tempor incididunt ut
+                        labore et dolore magna aliqua.</p>
+                    <!-- /wp:paragraph -->
+
+                    <!-- wp:paragraph {"textColor":"white"} -->
+                    <p class="has-white-color has-text-color"><a href="#">Čítať viac</a></p>
+                    <!-- /wp:paragraph --></div>
+                <!-- /wp:group --></div>
+            <!-- /wp:group --></div>
+        <!-- /wp:group -->
+
+        <!-- wp:group {"className":"col-12 col-sm-6"} -->
+        <div class="wp-block-group col-12 col-sm-6">
+            <!-- wp:group {"backgroundColor":"black","textColor":"white","className":"card"} -->
+            <div class="wp-block-group card has-white-color has-black-background-color has-text-color has-background">
+                <!-- wp:image -->
+                <figure class="wp-block-image"><img src="<?php echo placeholder_image_path(570, 356) ?>"/></figure>
+                <!-- /wp:image -->
+
+                <!-- wp:group {"className":"content-left"} -->
+                <div class="wp-block-group content-left">
+                    <!-- wp:heading {"level":3,"textColor":"white"} -->
+                    <h3 class="wp-block-heading has-white-color has-text-color">Nadpis</h3>
+                    <!-- /wp:heading -->
+
+                    <!-- wp:paragraph {"textColor":"white"} -->
+                    <p class="has-white-color has-text-color">Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                        sed do eiusmod tempor incididunt ut
+                        labore et dolore magna aliqua.</p>
+                    <!-- /wp:paragraph -->
+
+                    <!-- wp:paragraph {"textColor":"white"} -->
+                    <p class="has-white-color has-text-color">
+                        <a href="#">Čítať viac</a>
+                    </p>
+                    <!-- /wp:paragraph -->
+                </div>
+                <!-- /wp:group -->
+            </div>
+            <!-- /wp:group -->
+        </div>
+        <!-- /wp:group -->
+    </div>
+    <!-- /wp:group -->
+</div>
+<!-- /wp:group -->
---
+++ patterns/transparent-card.php
@@ -0,0 +1,74 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * Title: Priesvitné kartičky
+ * Slug: saleziani/transparent-cards
+ * Categories: saleziani
+ */
+?>
+
+<!-- wp:group {"className":"wp-pattern-saleziani-transparent-cards"} -->
+<div class="wp-block-group wp-pattern-saleziani-transparent-cards">
+    <!-- wp:group {"className":"row"} -->
+    <div class="wp-block-group row">
+        <!-- wp:group {"className":"col-12 col-sm-6"} -->
+        <div class="wp-block-group col-12 col-sm-6">
+            <!-- wp:group {"className":"card"} -->
+            <div class="wp-block-group card">
+                <!-- wp:image -->
+                <figure class="wp-block-image"><img src="<?php echo placeholder_image_path(256, 256) ?>"/></figure>
+                <!-- /wp:image -->
+                <!-- wp:group {"className":"content"} -->
+                <div class="wp-block-group content">
+                    <!-- wp:heading {"level":3} -->
+                    <h3 class="wp-block-heading">Nadpis</h3>
+                    <!-- /wp:heading -->
+
+                    <!-- wp:paragraph -->
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                        labore et dolore magna aliqua.</p>
+                    <!-- /wp:paragraph -->
+
+                    <!-- wp:paragraph -->
+                    <p><a href="#">Čítať viac</a></p>
+                    <!-- /wp:paragraph -->
+                </div>
+                <!-- /wp:group -->
+            </div>
+            <!-- /wp:group -->
+        </div>
+        <!-- /wp:group -->
+
+        <!-- wp:group {"className":"col-12 col-sm-6"} -->
+        <div class="wp-block-group col-12 col-sm-6">
+            <!-- wp:group {"className":"card"} -->
+            <div class="wp-block-group card">
+                <!-- wp:image -->
+                <figure class="wp-block-image"><img src="<?php echo placeholder_image_path(256, 256) ?>"/></figure>
+                <!-- /wp:image -->
+                <!-- wp:group {"className":"content"} -->
+                <div class="wp-block-group content">
+                    <!-- wp:heading {"level":3} -->
+                    <h3 class="wp-block-heading">Nadpis</h3>
+                    <!-- /wp:heading -->
+
+                    <!-- wp:paragraph -->
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                        labore et dolore magna aliqua.</p>
+                    <!-- /wp:paragraph -->
+
+                    <!-- wp:paragraph -->
+                    <p><a href="#">Čítať viac</a></p>
+                    <!-- /wp:paragraph -->
+                </div>
+                <!-- /wp:group -->
+            </div>
+            <!-- /wp:group -->
+        </div>
+        <!-- /wp:group -->
+    </div>
+    <!-- /wp:group -->
+</div>
+<!-- /wp:group -->
