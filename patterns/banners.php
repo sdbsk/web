1d49ac2a4f9ec4ffa8bd230807220c80906d0dcf
Pavol Tuka <pavol@bratia.sk>
2023-09-21 14:43:47 +0200

    Reformat

--- patterns/banners.php
+++ patterns/banners.php
@@ -22,12 +22,10 @@ declare(strict_types=1);
                     <!-- wp:heading {"level":3} -->
                     <h3 class="wp-block-heading">Nadpis</h3>
                     <!-- /wp:heading -->
-
                     <!-- wp:paragraph -->
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
                         labore et dolore magna aliqua.</p>
                     <!-- /wp:paragraph -->
-
                     <!-- wp:paragraph -->
                     <p><a href="#">Čítať viac</a></p>
                     <!-- /wp:paragraph -->
@@ -35,7 +33,6 @@ declare(strict_types=1);
                 <!-- /wp:group -->
             </div>
             <!-- /wp:group -->
-
             <!-- wp:group {"className":"col-12 col-sm-7","layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"center","orientation":"horizontal"}} -->
             <div class="wp-block-group col-12 col-sm-7">
                 <!-- wp:image -->
@@ -44,46 +41,42 @@ declare(strict_types=1);
             </div>
             <!-- /wp:group --><
             /div>
-        <!-- /wp:group -->
-        </div>
-    <!-- /wp:group -->
-
-    <!-- wp:group {"backgroundColor":"cappuccino", "className":"banner"} -->
-    <div class="wp-block-group banner has-cappuccino-background-color has-background">
-        <!-- wp:group {"className":"row align-items-center"} -->
-        <div class="wp-block-group row align-items-center">
-            <!-- wp:group {"className":"col-12 col-sm-7","layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"center","verticalAlignment":"center"}} -->
-            <div class="wp-block-group col-12 col-sm-7">
-                <!-- wp:image -->
-                <figure class="wp-block-image">
-                    <img src="<?php echo placeholder_image_path(300, 300) ?>"/>
-                </figure>
-                <!-- /wp:image -->
-            </div>
             <!-- /wp:group -->
-
-            <!-- wp:group {"className":"col-12 col-sm-5"} -->
-            <div class="wp-block-group col-12 col-sm-5">
-                <!-- wp:group {"className":"content-right"} -->
-                <div class="wp-block-group content-right">
-                    <!-- wp:heading {"level":3} -->
-                    <h3 class="wp-block-heading">Nadpis</h3>
-                    <!-- /wp:heading -->
-
-                    <!-- wp:paragraph -->
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-                        labore
-                        et dolore magna aliqua.</p>
-                    <!-- /wp:paragraph -->
-
-                    <!-- wp:paragraph -->
-                    <p><a href="#">Čítať viac</a></p>
-                    <!-- /wp:paragraph -->
+        </div>
+        <!-- /wp:group -->
+        <!-- wp:group {"backgroundColor":"cappuccino", "className":"banner"} -->
+        <div class="wp-block-group banner has-cappuccino-background-color has-background">
+            <!-- wp:group {"className":"row align-items-center"} -->
+            <div class="wp-block-group row align-items-center">
+                <!-- wp:group {"className":"col-12 col-sm-7","layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"center","verticalAlignment":"center"}} -->
+                <div class="wp-block-group col-12 col-sm-7">
+                    <!-- wp:image -->
+                    <figure class="wp-block-image">
+                        <img src="<?php echo placeholder_image_path(300, 300) ?>"/>
+                    </figure>
+                    <!-- /wp:image -->
+                </div>
+                <!-- /wp:group -->
+                <!-- wp:group {"className":"col-12 col-sm-5"} -->
+                <div class="wp-block-group col-12 col-sm-5">
+                    <!-- wp:group {"className":"content-right"} -->
+                    <div class="wp-block-group content-right">
+                        <!-- wp:heading {"level":3} -->
+                        <h3 class="wp-block-heading">Nadpis</h3>
+                        <!-- /wp:heading -->
+                        <!-- wp:paragraph -->
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                            labore
+                            et dolore magna aliqua.</p>
+                        <!-- /wp:paragraph -->
+                        <!-- wp:paragraph -->
+                        <p><a href="#">Čítať viac</a></p>
+                        <!-- /wp:paragraph -->
+                    </div>
+                    <!-- /wp:group -->
                 </div>
                 <!-- /wp:group -->
             </div>
-            <!-- /wp:group -->
-        </div>
+            <!-- /wp:group --></div>
         <!-- /wp:group --></div>
-    <!-- /wp:group --></div>
-<!-- /wp:group -->
+    <!-- /wp:group -->
