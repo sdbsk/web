cb8fdf4446c467e804ef86fcfd4d4523880731bd
Lukáš Tuka <lukas@bratia.sk>
2023-09-22 14:38:23 +0200

    Add colored cards.

--- patterns/colored-card.php
+++ patterns/colored-card.php
@@ -12,55 +12,83 @@ declare(strict_types=1);
 <!-- wp:group {"className":"wp-pattern-saleziani-colored-cards"} -->
 <div class="wp-block-group wp-pattern-saleziani-colored-cards"><!-- wp:group {"className":"row"} -->
     <div class="wp-block-group row"><!-- wp:group {"className":"col-12 col-sm-6"} -->
-        <div class="wp-block-group col-12 col-sm-6"><!-- wp:group {"backgroundColor":"potato","className":"card"} -->
-            <div class="wp-block-group card has-potato-background-color has-background">
-                <!-- wp:image -->
-                <figure class="wp-block-image"><img src="<?php echo placeholder_image_path(570, 356) ?>"/></figure>
-                <!-- /wp:image -->
+        <div class="wp-block-group col-12 col-sm-6"><!-- wp:group {"className":"card"} -->
+            <div class="wp-block-group card">
+                <!-- wp:group {"backgroundColor":"potato","className":"card-body "} -->
+                <div class="wp-block-group card-body has-potato-background-color has-background">
+                    <!-- wp:image -->
+                    <figure class="wp-block-image"><img src="<?php echo placeholder_image_path(570, 356) ?>"/></figure>
+                    <!-- /wp:image -->
 
-                <!-- wp:group {"className":"content-left"} -->
-                <div class="wp-block-group content-left"><!-- wp:heading {"level":3,"textColor":"white"} -->
-                    <h3 class="wp-block-heading has-white-color has-text-color">Nadpis</h3>
-                    <!-- /wp:heading -->
+                    <!-- wp:group {"className":"content-left"} -->
+                    <div class="wp-block-group content-left">
+                        <!-- wp:group {"className":"content-top"} -->
+                        <div class="wp-block-group content-top">
+                            <!-- wp:heading {"level":3,"textColor":"white"} -->
+                            <h3 class="wp-block-heading has-white-color has-text-color">Nadpis</h3>
+                            <!-- /wp:heading -->
 
-                    <!-- wp:paragraph {"textColor":"white"} -->
-                    <p class="has-white-color has-text-color">Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                        sed do eiusmod tempor incididunt ut
-                        labore et dolore magna aliqua.</p>
-                    <!-- /wp:paragraph -->
+                            <!-- wp:paragraph {"textColor":"white"} -->
+                            <p class="has-white-color has-text-color">Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                                sed do eiusmod tempor incididunt ut
+                                labore et dolore magna aliqua.</p>
+                            <!-- /wp:paragraph -->
+                        </div>
+                        <!-- /wp:group -->
 
-                    <!-- wp:paragraph {"textColor":"white"} -->
-                    <p class="has-white-color has-text-color"><a href="#">Čítať viac</a></p>
-                    <!-- /wp:paragraph --></div>
-                <!-- /wp:group --></div>
-            <!-- /wp:group --></div>
+                        <!-- wp:group {"className":"content-bottom"} -->
+                        <div class="wp-block-group content-bottom">
+                            <!-- wp:paragraph {"textColor":"white"} -->
+                            <p class="has-white-color has-text-color"><a href="#">Čítať viac</a></p>
+                            <!-- /wp:paragraph -->
+                        </div>
+                        <!-- /wp:group -->
+                    </div>
+                    <!-- /wp:group -->
+                </div>
+                <!-- /wp:group -->
+            </div>
+            <!-- /wp:group -->
+        </div>
         <!-- /wp:group -->
 
         <!-- wp:group {"className":"col-12 col-sm-6"} -->
         <div class="wp-block-group col-12 col-sm-6">
-            <!-- wp:group {"backgroundColor":"black","textColor":"white","className":"card"} -->
-            <div class="wp-block-group card has-white-color has-black-background-color has-text-color has-background">
-                <!-- wp:image -->
-                <figure class="wp-block-image"><img src="<?php echo placeholder_image_path(570, 356) ?>"/></figure>
-                <!-- /wp:image -->
+            <!-- wp:group {"className":"card"} -->
+            <div class="wp-block-group card">
+                <!-- wp:group {"backgroundColor":"black","textColor":"white", "className":"card-body"} -->
+                <div class="wp-block-group card-body has-white-color has-black-background-color has-text-color has-background">
+                    <!-- wp:image -->
+                    <figure class="wp-block-image"><img src="<?php echo placeholder_image_path(570, 356) ?>"/></figure>
+                    <!-- /wp:image -->
 
-                <!-- wp:group {"className":"content-left"} -->
-                <div class="wp-block-group content-left">
-                    <!-- wp:heading {"level":3,"textColor":"white"} -->
-                    <h3 class="wp-block-heading has-white-color has-text-color">Nadpis</h3>
-                    <!-- /wp:heading -->
+                    <!-- wp:group {"className":"content-left"} -->
+                    <div class="wp-block-group content-left">
+                        <!-- wp:group {"className":"content-top"} -->
+                        <div class="wp-block-group content-top">
+                            <!-- wp:heading {"level":3,"textColor":"white"} -->
+                            <h3 class="wp-block-heading has-white-color has-text-color">Nadpis</h3>
+                            <!-- /wp:heading -->
 
-                    <!-- wp:paragraph {"textColor":"white"} -->
-                    <p class="has-white-color has-text-color">Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                        sed do eiusmod tempor incididunt ut
-                        labore et dolore magna aliqua.</p>
-                    <!-- /wp:paragraph -->
+                            <!-- wp:paragraph {"textColor":"white"} -->
+                            <p class="has-white-color has-text-color">Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                                sed do eiusmod tempor incididunt ut
+                                labore et dolore magna aliqua.</p>
+                            <!-- /wp:paragraph -->
+                        </div>
+                        <!-- /wp:group -->
 
-                    <!-- wp:paragraph {"textColor":"white"} -->
-                    <p class="has-white-color has-text-color">
-                        <a href="#">Čítať viac</a>
-                    </p>
-                    <!-- /wp:paragraph -->
+                        <!-- wp:group {"className":"content-bottom"} -->
+                        <div class="wp-block-group content-bottom">
+                            <!-- wp:paragraph {"textColor":"white"} -->
+                            <p class="has-white-color has-text-color">
+                                <a href="#">Čítať viac</a>
+                            </p>
+                            <!-- /wp:paragraph -->
+                        </div>
+                        <!-- /wp:group -->
+                    </div>
+                    <!-- /wp:group -->
                 </div>
                 <!-- /wp:group -->
             </div>
