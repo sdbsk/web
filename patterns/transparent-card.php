58c5810fd52e49780758bc57eb939e1a98d9e8e8
Lukáš Tuka <lukas@bratia.sk>
2023-09-25 14:57:11 +0200

    Add transparent cards.

--- patterns/transparent-card.php
+++ patterns/transparent-card.php
@@ -17,51 +17,70 @@ declare(strict_types=1);
         <div class="wp-block-group col-12 col-sm-6">
             <!-- wp:group {"className":"card"} -->
             <div class="wp-block-group card">
-                <!-- wp:image -->
-                <figure class="wp-block-image"><img src="<?php echo placeholder_image_path(256, 256) ?>"/></figure>
-                <!-- /wp:image -->
-                <!-- wp:group {"className":"content"} -->
-                <div class="wp-block-group content">
-                    <!-- wp:heading {"level":3} -->
-                    <h3 class="wp-block-heading">Nadpis</h3>
-                    <!-- /wp:heading -->
-
-                    <!-- wp:paragraph -->
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-                        labore et dolore magna aliqua.</p>
-                    <!-- /wp:paragraph -->
-
-                    <!-- wp:paragraph -->
-                    <p><a href="#">Čítať viac</a></p>
-                    <!-- /wp:paragraph -->
+                <!-- wp:group {"className":"card-body "} -->
+                <div class="wp-block-group card-body">
+                    <!-- wp:image -->
+                    <figure class="wp-block-image"><img src="<?php echo placeholder_image_path(256, 256) ?>"/></figure>
+                    <!-- /wp:image -->
+                    <!-- wp:group {"className":"content"} -->
+                    <div class="wp-block-group content">
+                        <!-- wp:group {"className":"content-top"} -->
+                        <div class="wp-block-group content-top">
+                            <!-- wp:heading {"level":3} -->
+                            <h3 class="wp-block-heading">Nadpis</h3>
+                            <!-- /wp:heading -->
+                            <!-- wp:paragraph -->
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                                labore et dolore magna aliqua.</p>
+                            <!-- /wp:paragraph -->
+                        </div>
+                        <!-- /wp:group -->
+                        <!-- wp:group {"className":"content-bottom"} -->
+                        <div class="wp-block-group content-bottom">
+                            <!-- wp:paragraph -->
+                            <p><a href="#">Čítať viac</a></p>
+                            <!-- /wp:paragraph -->
+                        </div>
+                        <!-- /wp:group -->
+                    </div>
+                    <!-- /wp:group -->
                 </div>
                 <!-- /wp:group -->
             </div>
             <!-- /wp:group -->
         </div>
         <!-- /wp:group -->
-
         <!-- wp:group {"className":"col-12 col-sm-6"} -->
         <div class="wp-block-group col-12 col-sm-6">
             <!-- wp:group {"className":"card"} -->
             <div class="wp-block-group card">
-                <!-- wp:image -->
-                <figure class="wp-block-image"><img src="<?php echo placeholder_image_path(256, 256) ?>"/></figure>
-                <!-- /wp:image -->
-                <!-- wp:group {"className":"content"} -->
-                <div class="wp-block-group content">
-                    <!-- wp:heading {"level":3} -->
-                    <h3 class="wp-block-heading">Nadpis</h3>
-                    <!-- /wp:heading -->
-
-                    <!-- wp:paragraph -->
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-                        labore et dolore magna aliqua.</p>
-                    <!-- /wp:paragraph -->
-
-                    <!-- wp:paragraph -->
-                    <p><a href="#">Čítať viac</a></p>
-                    <!-- /wp:paragraph -->
+                <!-- wp:group {"className":"card-body "} -->
+                <div class="wp-block-group card-body">
+                    <!-- wp:image -->
+                    <figure class="wp-block-image"><img src="<?php echo placeholder_image_path(256, 256) ?>"/></figure>
+                    <!-- /wp:image -->
+                    <!-- wp:group {"className":"content"} -->
+                    <div class="wp-block-group content">
+                        <!-- wp:group {"className":"content-top"} -->
+                        <div class="wp-block-group content-top">
+                            <!-- wp:heading {"level":3} -->
+                            <h3 class="wp-block-heading">Nadpis</h3>
+                            <!-- /wp:heading -->
+                            <!-- wp:paragraph -->
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                                labore et dolore magna aliqua.</p>
+                            <!-- /wp:paragraph -->
+                        </div>
+                        <!-- /wp:group -->
+                        <!-- wp:group {"className":"content-bottom"} -->
+                        <div class="wp-block-group content-bottom">
+                            <!-- wp:paragraph -->
+                            <p><a href="#">Čítať viac</a></p>
+                            <!-- /wp:paragraph -->
+                        </div>
+                        <!-- /wp:group -->
+                    </div>
+                    <!-- /wp:group -->
                 </div>
                 <!-- /wp:group -->
             </div>
