ba42f172d1a4b2024bea511e47c877fc7862c8dd
Pavol Tuka <pavol@bratia.sk>
2023-09-26 15:52:17 +0200

    Remove inline style from three-cards.php

--- patterns/three-cards.php
+++ patterns/three-cards.php
@@ -17,25 +17,22 @@ declare(strict_types=1);
         <div class="wp-block-group col-12 col-md-4 col-sm-6">
             <!-- wp:group {"className":"wp-saleziani-basic-card"} -->
             <div class="wp-block-group wp-saleziani-basic-card">
-                <!-- wp:group {"style":{"color":{"background":"#e1ecf8"}},"className":"card-body"} -->
-                <div class="wp-block-group card-body has-background" style="background-color:#e1ecf8">
+                <!-- wp:group {"backgroundColor":"light-blue","className":"card-body"} -->
+                <div class="wp-block-group card-body has-light-blue-background-color has-background">
                     <!-- wp:image -->
                     <figure class="wp-block-image">
                         <img src="<?php echo placeholder_image_path(320, 160) ?>" alt=""/>
                     </figure>
                     <!-- /wp:image -->
-
                     <!-- wp:group {"className":"content"} -->
                     <div class="wp-block-group content"><!-- wp:group {"className":"text"} -->
                         <div class="wp-block-group text"><!-- wp:heading {"textAlign":"left","level":3} -->
                             <h3 class="wp-block-heading has-text-align-left"><a href="#">Ľubovoľný nadpis</a></h3>
                             <!-- /wp:heading -->
-
                             <!-- wp:paragraph {"className":"text"} -->
                             <p class="text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco</p>
                             <!-- /wp:paragraph --></div>
                         <!-- /wp:group -->
-
                         <!-- wp:paragraph -->
                         <p><a href="#" target="_blank" rel="noreferrer noopener">www.odkaz.niekam.sk</a></p>
                         <!-- /wp:paragraph -->
@@ -51,25 +48,22 @@ declare(strict_types=1);
         <div class="wp-block-group col-12 col-md-4 col-sm-6">
             <!-- wp:group {"className":"wp-saleziani-basic-card"} -->
             <div class="wp-block-group wp-saleziani-basic-card">
-                <!-- wp:group {"style":{"color":{"background":"#e1ecf8"}},"className":"card-body"} -->
-                <div class="wp-block-group card-body has-background" style="background-color:#e1ecf8">
+                <!-- wp:group {"backgroundColor":"light-blue","className":"card-body"} -->
+                <div class="wp-block-group card-body has-light-blue-background-color has-background">
                     <!-- wp:image -->
                     <figure class="wp-block-image">
                         <img src="<?php echo placeholder_image_path(320, 160) ?>" alt=""/>
                     </figure>
                     <!-- /wp:image -->
-
                     <!-- wp:group {"className":"content"} -->
                     <div class="wp-block-group content"><!-- wp:group {"className":"text"} -->
                         <div class="wp-block-group text"><!-- wp:heading {"textAlign":"left","level":3} -->
                             <h3 class="wp-block-heading has-text-align-left"><a href="#">Ľubovoľný nadpis</a></h3>
                             <!-- /wp:heading -->
-
                             <!-- wp:paragraph {"className":"text"} -->
                             <p class="text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco</p>
                             <!-- /wp:paragraph --></div>
                         <!-- /wp:group -->
-
                         <!-- wp:paragraph -->
                         <p><a href="#" target="_blank" rel="noreferrer noopener">www.odkaz.niekam.sk</a></p>
                         <!-- /wp:paragraph -->
@@ -85,25 +79,22 @@ declare(strict_types=1);
         <div class="wp-block-group col-12 col-md-4 col-sm-6">
             <!-- wp:group {"className":"wp-saleziani-basic-card"} -->
             <div class="wp-block-group wp-saleziani-basic-card">
-                <!-- wp:group {"style":{"color":{"background":"#e1ecf8"}},"className":"card-body"} -->
-                <div class="wp-block-group card-body has-background" style="background-color:#e1ecf8">
+                <!-- wp:group {"backgroundColor":"light-blue","className":"card-body"} -->
+                <div class="wp-block-group card-body has-light-blue-background-color has-background">
                     <!-- wp:image -->
                     <figure class="wp-block-image">
                         <img src="<?php echo placeholder_image_path(320, 160) ?>" alt=""/>
                     </figure>
                     <!-- /wp:image -->
-
                     <!-- wp:group {"className":"content"} -->
                     <div class="wp-block-group content"><!-- wp:group {"className":"text"} -->
                         <div class="wp-block-group text"><!-- wp:heading {"textAlign":"left","level":3} -->
                             <h3 class="wp-block-heading has-text-align-left"><a href="#">Ľubovoľný nadpis</a></h3>
                             <!-- /wp:heading -->
-
                             <!-- wp:paragraph {"className":"text"} -->
                             <p class="text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco</p>
                             <!-- /wp:paragraph --></div>
                         <!-- /wp:group -->
-
                         <!-- wp:paragraph -->
                         <p><a href="#" target="_blank" rel="noreferrer noopener">www.odkaz.niekam.sk</a></p>
                         <!-- /wp:paragraph -->
@@ -119,8 +110,3 @@ declare(strict_types=1);
     <!-- /wp:group -->
 </div>
 <!-- /wp:group -->
-
-
-
-
-
