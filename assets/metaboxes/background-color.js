ead84bc1a48017a20eb5ba8c9bd518a408d071b4
Pavol Tuka <pavol@bratia.sk>
2023-09-26 15:35:33 +0200

    Add background metabox, page-background pattern

---
+++ assets/metaboxes/background-color.js
@@ -0,0 +1,26 @@
+(wp => {
+    if ('page' === window.pagenow) {
+        wp.plugins.registerPlugin('background-color', {
+            render: () => {
+                const colors = wp.data.useSelect('core/block-editor').getSettings().colors;
+                const meta = wp.data.useSelect((select) => select('core/editor').getEditedPostAttribute('meta')) ?? {};
+                const {editPost} = wp.data.useDispatch('core/editor');
+                const [backgroundColor, setBackgroundColor] = wp.element.useState(meta.background_color);
+
+                wp.element.useEffect(() => {
+                    editPost({meta: {...meta, background_color: backgroundColor}});
+                }, [backgroundColor]);
+
+                return wp.element.createElement(wp.editPost.PluginDocumentSettingPanel, {title: 'Farba pozadia'},
+                    wp.element.createElement(wp.components.ColorPalette, {
+                        colors: colors,
+                        clearable: true,
+                        disableCustomColors: true,
+                        onChange: (value, index) => setBackgroundColor(undefined === index ? '' : colors[index].slug),
+                        value: backgroundColor
+                    })
+                );
+            }
+        });
+    }
+})(window.wp);
