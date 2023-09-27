4d35f628ce780d62cfaa1081159cea4c188e9e36
“Martin <martinlikavcan@gmail.com>
2023-09-27 12:24:27 +0200

    add copyright to footer, add heading description link pattern...

---
+++ patterns/page-copyright.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * Title: Copyright
+ * Slug: saleziani/page-copyright
+ * Categories: saleziani
+ * Inserter: false
+ */
+?>
+
+<!-- wp:group {"className":"wp-pattern-page-copyright"} -->
+<div class="wp-block-group wp-pattern-page-copyright">&copy;&nbsp;<?php echo date('Y') ?> Saleziáni don&nbsp;Bosca</div>
+<!-- /wp:group -->
+
