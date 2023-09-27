e866376cb8c1c91a7ed5c33d96a146cfd6064282
Pavol Tuka <pavol@bratia.sk>
2023-09-27 07:41:51 +0200

    Improve page-background metabox and pattern

--- assets/metaboxes/background-color.js
+++ assets/metaboxes/background-color.js
@@ -2,11 +2,17 @@
     if ('page' === window.pagenow) {
         wp.plugins.registerPlugin('background-color', {
             render: () => {
+                const canvas = document.querySelector('.editor-styles-wrapper');
                 const colors = wp.data.useSelect('core/block-editor').getSettings().colors;
                 const meta = wp.data.useSelect((select) => select('core/editor').getEditedPostAttribute('meta')) ?? {};
                 const {editPost} = wp.data.useDispatch('core/editor');
                 const [backgroundColor, setBackgroundColor] = wp.element.useState(meta.background_color);
-                const selectedBackgroundColor = colors.filter((c) => backgroundColor === c.slug)[0];
+                const selectedBackground = colors.filter((c) => backgroundColor === c.slug)[0];
+                const selectedBackgroundColor = undefined === selectedBackground ? null : selectedBackground.color;
+
+                if (canvas instanceof Element) {
+                    canvas.style.backgroundColor = selectedBackgroundColor;
+                }
 
                 wp.element.useEffect(() => {
                     editPost({meta: {...meta, background_color: backgroundColor}});
@@ -17,8 +23,11 @@
                         colors: colors,
                         clearable: true,
                         disableCustomColors: true,
-                        onChange: (value, index) => setBackgroundColor(undefined === index ? '' : colors[index].slug),
-                        value: undefined === selectedBackgroundColor ? '' : selectedBackgroundColor.color
+                        onChange: (value, index) => {
+                            setBackgroundColor(undefined === index ? null : colors[index].slug);
+                            canvas.style.backgroundColor = value ?? null;
+                        },
+                        value: selectedBackgroundColor
                     })
                 );
             }
