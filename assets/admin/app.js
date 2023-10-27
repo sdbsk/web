be472cb0a9e4a30d5ce5ae1f93d18a90345e1d30
Pavol Tuka <pavol@bratia.sk>
2023-10-27 10:01:02 +0200

    Add narrow template, remove background-color plugin

--- assets/admin/app.js
+++ assets/admin/app.js
@@ -3,8 +3,6 @@ import './blocks/link-to-page';
 import './blocks/navigation';
 import './blocks/newsletter-form';
 
-import './plugins/background-color';
-
 import './scripts/append-template-class-to-post-title-and-post-content';
 import './scripts/disable-default-category-checkbox-in-edit-form';
 import './scripts/disable-default-category-checkbox-in-quick-edit-form';
