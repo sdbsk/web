4f28578a424d84ddd1822ee71bcf25d8687ea3a9
Lukáš Tuka <lukas@bratia.sk>
2023-10-27 13:55:38 +0200

    Change heading-description-link styles.

--- patterns/heading-description-link.php
+++ patterns/heading-description-link.php
@@ -11,25 +11,17 @@ declare(strict_types=1);
 
 <!-- wp:group {"className":"wp-pattern-saleziani-heading-description-link"} -->
 <div class="wp-block-group wp-pattern-saleziani-heading-description-link">
-    <!-- wp:group {"className":"row"} -->
-    <div class="wp-block-group row">
-        <!-- wp:group {"className":"col-12 col-md-8"} -->
-        <div class="wp-block-group col-12 col-md-8">
-            <!-- wp:heading {"className":"mb-3"} -->
-            <h2 class="wp-block-heading mb-3">Nadpis</h2>
-            <!-- /wp:heading -->
-            <!-- wp:paragraph -->
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean at cursus arcu, id varius dui. Ut
-                tristique, lorem a consequat varius, sapien urna volutpat sem</p>
-            <!-- /wp:paragraph -->
-            <!-- wp:paragraph -->
-            <p>
-                <a href="#">Zobraziť všetko</a>
-            </p>
-            <!-- /wp:paragraph -->
-        </div>
-        <!-- /wp:group -->
-    </div>
-    <!-- /wp:group -->
+    <!-- wp:heading {"className":"mb-3"} -->
+    <h2 class="wp-block-heading mb-3">Nadpis</h2>
+    <!-- /wp:heading -->
+    <!-- wp:paragraph -->
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean at cursus arcu, id varius dui. Ut
+        tristique, lorem a consequat varius, sapien urna volutpat sem</p>
+    <!-- /wp:paragraph -->
+    <!-- wp:paragraph -->
+    <p>
+        <a href="#">Zobraziť všetko</a>
+    </p>
+    <!-- /wp:paragraph -->
 </div>
 <!-- /wp:group -->
