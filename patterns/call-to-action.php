fe5fdc386481f9e0187c7215cc09f6ba4b78ee24
“Martin <martinlikavcan@gmail.com>
2023-10-30 09:39:22 +0100

    call to action

--- patterns/call-to-action.php
+++ patterns/call-to-action.php
@@ -9,31 +9,55 @@ declare(strict_types=1);
  */
 ?>
 
-<!-- wp:group {"backgroundColor":"light-blue","className":"wp-pattern-saleziani-call-to-action"} -->
-<div class="has-light-blue-background-color has-background wp-block-group wp-pattern-saleziani-call-to-action">
-    <!-- wp:group -->
-    <div class="wp-block-group">
-        <!-- wp:heading -->
-        <h2 class="wp-block-heading">Vaša podpora je dôležitá</h2>
-        <!-- /wp:heading -->
-        <!-- wp:paragraph -->
-        <p>Saleziánske dielo je sieť saleziánskych komunitných centier, v ktorých tisíce ľudí denne trávia zmysluplný čas. Každý je vítaný. Tvoj pravidelný mesačný príspevok je potrebný pre udržanie a rozvoj športových, kultúrnych, sociálnych a duchovných aktivít pre deti, mladých, rodičov aj seniorov.</p>
-        <!-- /wp:paragraph -->
-        <!-- wp:buttons -->
-        <div class="wp-block-buttons">
-            <!-- wp:button -->
-            <div class="wp-block-button">
-                <a class="wp-block-button__link wp-element-button" href="#">Podporiť teraz</a>
+
+<!-- wp:group {"backgroundColor":"c7e7f1","className":"wp-pattern-saleziani-call-to-action"} -->
+<div class="wp-block-group wp-pattern-saleziani-call-to-action has-c-7-e-7-f-1-background-color has-background">
+    <!-- wp:group {"className":"row"} -->
+    <div class="wp-block-group row">
+        <!-- wp:group {"className":"col-md-8"} -->
+        <div class="wp-block-group col-md-8">
+            <!-- wp:group {"className":"content"} -->
+            <div class="wp-block-group content">
+                <!-- wp:heading {"textColor":"272727"} -->
+                <h2 class="wp-block-heading has-272727-color has-text-color">Vaša podpora je dôležitá</h2>
+                <!-- /wp:heading -->
+                <!-- wp:paragraph {"textColor":"272727"} -->
+                <p class="has-272727-color has-text-color">Saleziánske dielo je sieť saleziánskych komunitných centier,
+                    v ktorých tisíce ľudí denne trávia
+                    zmysluplný
+                    čas. Každý je vítaný. Tvoj pravidelný mesačný príspevok je potrebný pre udržanie a rozvoj
+                    športových,
+                    kultúrnych, sociálnych a duchovných aktivít pre deti, mladých, rodičov aj seniorov.</p>
+                <!-- /wp:paragraph -->
+                <!-- wp:buttons -->
+                <div class="wp-block-buttons">
+                    <!-- wp:button {"backgroundColor":"cf3942"} -->
+                    <div class="wp-block-button">
+                        <a class="wp-block-button__link has-cf-3942-background-color has-background wp-element-button"
+                           href="#">
+                            Podporiť teraz
+                        </a></div>
+                    <!-- /wp:button -->
+                </div>
+                <!-- /wp:buttons -->
             </div>
-            <!-- /wp:button -->
+
         </div>
-        <!-- /wp:buttons -->
+        <!-- /wp:group -->
     </div>
     <!-- /wp:group -->
-    <!-- wp:image -->
-    <figure class="wp-block-image">
-        <img src="<?php echo placeholder_image_path(800, 800) ?>" alt=""/>
-    </figure>
-    <!-- /wp:image -->
+
+    <!-- wp:group {"className":"col-md-4"} -->
+    <div class="wp-block-group col-md-4">
+        <!-- wp:image -->
+        <figure class="wp-block-image">
+            <img src="<?php echo placeholder_image_path(500, 500) ?>" alt=""/>
+        </figure>
+        <!-- /wp:image -->
+    </div>
+    <!-- /wp:group -->
 </div>
 <!-- /wp:group -->
+</div>
+<!-- /wp:group -->
+
