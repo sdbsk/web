3917fe830adc5ac98a135fa59ab4f846054351c5
“Martin <martinlikavcan@gmail.com>
2023-11-03 12:21:01 +0100

    link list, page template, footer, buttons, content pattern

---
+++ patterns/content-heading-text.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * Title: Obsahový blok s nadpisom a textom
+ * Slug: saleziani/content-heading-text
+ * Categories: saleziani
+ */
+?>
+
+<!-- wp:group {"className":"wp-pattern-saleziani-content-heading-text"} -->
+<div class="wp-block-group wp-pattern-saleziani-content-heading-text">
+    <!-- wp:heading {"level":3} -->
+    <h3 class="wp-block-heading">Lorem ipsum dolor sit amet</h3>
+    <!-- /wp:heading -->
+
+    <!-- wp:paragraph -->
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+        magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+        consequat.</p>
+    <!-- /wp:paragraph -->
+</div>
+<!-- /wp:group -->
