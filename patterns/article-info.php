750771d28d4cb84e14f80d2e6774eefc54366030
“Martin <martinlikavcan@gmail.com>
2023-11-06 10:33:21 +0100

    article info ...

---
+++ patterns/article-info.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * Title: Informácie o článku
+ * Slug: saleziani/article-info
+ * Categories: saleziani
+ */
+?>
+
+<!-- wp:group {"className":"wp-pattern-saleziani-article-info"} -->
+<div class="wp-block-group wp-pattern-saleziani-article-info">
+    <!-- wp:post-terms {"term":"category","separator": ""} /-->
+    <!-- wp:post-date /-->
+</div>
+<!-- /wp:group -->
+
--- patterns/numbers.php
+++ patterns/numbers.php
@@ -18,42 +18,58 @@ declare(strict_types=1);
     <div class="wp-block-group row row-cols-1 row-cols-sm-2 row-cols-md-4 g-4 justify-content-center">
         <!-- wp:group {"className":"col"} -->
         <div class="wp-block-group col">
-            <!-- wp:heading {"level":3,"textColor":"cf3942"} -->
-            <h3 class="wp-block-heading has-cf-3942-color has-text-color">200</h3>
-            <!-- /wp:heading -->
-            <!-- wp:paragraph {"textColor":"cf3942"} -->
-            <p class="has-cf-3942-color has-text-color">saleziánov</p>
-            <!-- /wp:paragraph -->
+            <!-- wp:group {"className":"number-box"} -->
+            <div class="wp-block-group number-box">
+                <!-- wp:heading {"level":3,"textColor":"cf3942"} -->
+                <h3 class="wp-block-heading has-cf-3942-color has-text-color">200</h3>
+                <!-- /wp:heading -->
+                <!-- wp:paragraph {"textColor":"cf3942"} -->
+                <p class="has-cf-3942-color has-text-color">saleziánov</p>
+                <!-- /wp:paragraph -->
+            </div>
+            <!-- /wp:group -->
         </div>
         <!-- /wp:group -->
         <!-- wp:group {"className":"col"} -->
         <div class="wp-block-group col">
-            <!-- wp:heading {"level":3,"textColor":"966a4e"} -->
-            <h3 class="wp-block-heading has-966-a-4-e-color has-text-color">24 165</h3>
-            <!-- /wp:heading -->
-            <!-- wp:paragraph {"textColor":"966a4e"} -->
-            <p class="has-966-a-4-e-color has-text-color">detí na táboroch</p>
-            <!-- /wp:paragraph -->
+            <!-- wp:group {"className":"number-box"} -->
+            <div class="wp-block-group number-box">
+                <!-- wp:heading {"level":3,"textColor":"966a4e"} -->
+                <h3 class="wp-block-heading has-966-a-4-e-color has-text-color">24 165</h3>
+                <!-- /wp:heading -->
+                <!-- wp:paragraph {"textColor":"966a4e"} -->
+                <p class="has-966-a-4-e-color has-text-color">detí na táboroch</p>
+                <!-- /wp:paragraph -->
+            </div>
+            <!-- /wp:group -->
         </div>
         <!-- /wp:group -->
         <!-- wp:group {"className":"col"} -->
         <div class="wp-block-group col">
-            <!-- wp:heading {"level":3,"textColor":"facb3e"} -->
-            <h3 class="wp-block-heading has-facb-3-e-color has-text-color">1 050</h3>
-            <!-- /wp:heading -->
-            <!-- wp:paragraph {"textColor":"facb3e"} -->
-            <p class="has-facb-3-e-color has-text-color">podujatí</p>
-            <!-- /wp:paragraph -->
+            <!-- wp:group {"className":"number-box"} -->
+            <div class="wp-block-group number-box">
+                <!-- wp:heading {"level":3,"textColor":"facb3e"} -->
+                <h3 class="wp-block-heading has-facb-3-e-color has-text-color">1 050</h3>
+                <!-- /wp:heading -->
+                <!-- wp:paragraph {"textColor":"facb3e"} -->
+                <p class="has-facb-3-e-color has-text-color">podujatí</p>
+                <!-- /wp:paragraph -->
+            </div>
+            <!-- /wp:group -->
         </div>
         <!-- /wp:group -->
         <!-- wp:group {"className":"col"} -->
         <div class="wp-block-group col">
-            <!-- wp:heading {"level":3,"textColor":"50b6b1"} -->
-            <h3 class="wp-block-heading has-50-b-6-b-1-color has-text-color">17</h3>
-            <!-- /wp:heading -->
-            <!-- wp:paragraph {"textColor":"50b6b1"} -->
-            <p class="has-50-b-6-b-1-color has-text-color">projektov</p>
-            <!-- /wp:paragraph -->
+            <!-- wp:group {"className":"number-box"} -->
+            <div class="wp-block-group number-box">
+                <!-- wp:heading {"level":3,"textColor":"50b6b1"} -->
+                <h3 class="wp-block-heading has-50-b-6-b-1-color has-text-color">17</h3>
+                <!-- /wp:heading -->
+                <!-- wp:paragraph {"textColor":"50b6b1"} -->
+                <p class="has-50-b-6-b-1-color has-text-color">projektov</p>
+                <!-- /wp:paragraph -->
+            </div>
+            <!-- /wp:group -->
         </div>
         <!-- /wp:group -->
     </div>
