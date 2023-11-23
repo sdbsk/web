e18168f273a5c3380144cd7eeda035c50dddec7e
Pavol Tuka <pavol@bratia.sk>
2023-11-23 13:42:51 +0100

    Add composer.lock

--- build-icons.js
+++ build-icons.js
@@ -3,4 +3,4 @@ const fs = require('fs');
 fs.writeFileSync('assets/admin/scripts/icon-names.js', `export default ${JSON.stringify(
     fs.readdirSync('vendor/google/material-symbols/svg/300/outlined/')
         .filter(file => !file.match(/-fill\.svg$/))
-        .map(file => file.match(/^([^.]+)\./)[1]), null, 2)};`);
+        .map(file => file.match(/^([^.]+)\./)[1]), null, 2)};\n`);
