43d0664c2ce1380fc6431961880b41e2888a5b00
Pavol Tuka <pavol@bratia.sk>
2024-01-11 10:37:12 +0100

    Simplify deploy.php

--- deploy.php
+++ deploy.php
@@ -18,27 +18,23 @@ set('shared_dirs', ['web/app/uploads', 'web/app/w3tc-config']);
 set('writable_dirs', []);
 set('allow_anonymous_stats', false);
 
-task('deploy:install_languages', function (): void {
+task('deploy:theme', function (): void {
     run('cd {{ release_path }} && php8.1 bin/wp-cli.phar language core install en_US sk_SK');
     run('cd {{ release_path }} && php8.1 bin/wp-cli.phar language plugin install --all en_US sk_SK');
 });
 
-task('deploy:flush_rewrite', function (): void {
-    run('cd {{ release_path }} && php8.1 bin/wp-cli.phar rewrite flush --hard');
-});
-
-task('deploy:flush_cache', function (): void {
+task('deploy:flush', function (): void {
     run('cd {{ release_path }} && php8.1 bin/wp-cli.phar cache flush all');
     run('cd {{ release_path }} && php8.1 bin/wp-cli.phar w3-total-cache flush all');
+    run('cd {{ release_path }} && php8.1 bin/wp-cli.phar rewrite flush --hard');
 });
 
 task('deploy', [
     'deploy:prepare',
     'deploy:vendors',
-    'deploy:install_languages',
-    'deploy:flush_rewrite',
+    'deploy:theme',
+    'deploy:flush',
     'deploy:publish',
-    'deploy:flush_cache',
 ]);
 
 after('deploy:failed', 'deploy:unlock');
