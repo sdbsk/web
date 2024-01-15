14df7f4f284a114d87b4ac36f75bcf9ada5c9040
Pavol Tuka <pavol@bratia.sk>
2024-01-15 13:50:41 +0100

    Rollback: Reset opcache after deploy

--- deploy.php
+++ deploy.php
@@ -29,11 +29,6 @@ task('deploy:flush', function (): void {
     run('cd {{ release_path }} && php8.1 bin/wp-cli.phar rewrite flush --hard');
 });
 
-task('deploy:opcache_reset', function (): void {
-    $filename = uniqid('web/opcache-reset-') . '.php';
-    run('cd {{ release_path }} && echo "<?php opcache_reset();" > ' . $filename . ' && curl https://stage.saleziani.sk/' . $filename);
-});
-
 task('deploy', [
     'deploy:prepare',
     'deploy:vendors',
@@ -42,5 +37,4 @@ task('deploy', [
     'deploy:publish',
 ]);
 
-after('deploy:symlink', 'deploy:opcache_reset');
 after('deploy:failed', 'deploy:unlock');
