c848d34e3e9e1340c06f3af8d2891e81dd2c373c
“Filip <filip@bratia.sk>
2024-02-04 21:11:18 +0100

    Disable wp-cron

--- .htaccess.php
+++ .htaccess.php
@@ -4,7 +4,7 @@
 
 add_filter('mod_rewrite_rules', function($rules) {
     return $rules . <<<HTACCESS
-<FilesMatch (xmlrpc\.php|readme\.html)>
+<FilesMatch (xmlrpc\.php|readme\.html|wp-cron\.php)>
     Order deny,allow
     Deny from all
 </FilesMatch>
--- config/application.php
+++ config/application.php
@@ -109,7 +109,7 @@ Config::define('NONCE_SALT', env('NONCE_SALT'));
  * Custom Settings
  */
 Config::define('AUTOMATIC_UPDATER_DISABLED', true);
-Config::define('DISABLE_WP_CRON', env('DISABLE_WP_CRON') ?: false);
+Config::define('DISABLE_WP_CRON', true);
 
 // Disable the plugin and theme file editor in the admin
 Config::define('DISALLOW_FILE_EDIT', true);
