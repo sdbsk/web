6d0786b56b98ceac7226416fcb5b683d1ad6a8fe
“Martin <martinlikavcan@gmail.com>
2024-12-10 14:34:06 +0100

    fix numbers and add dicaprios pattern

---
+++ src/legacy/patterns/dicaprio-image-text.php
@@ -0,0 +1,43 @@
+<?php
+
+declare( strict_types=1 );
+
+/**
+ * Title: Dicaprio image text
+ * Slug: saleziani/dicaprio-image-text
+ * Categories: saleziani
+ */
+?>
+
+<!-- wp:generateblocks/container {"uniqueId":"a7fc104b","isDynamic":true,"blockVersion":4,"spacing":{"marginBottom":"8rem","marginBottomTablet":"4rem"},"metadata":{"categories":["saleziani"],"patternName":"saleziani/dicaprio-image-text","name":"Dicaprio text image"}} -->
+<!-- wp:generateblocks/grid {"uniqueId":"63cf8cfe","columns":2,"horizontalGap":60,"isDynamic":true,"blockVersion":3} -->
+<!-- wp:generateblocks/container {"uniqueId":"9f2f9a3e","isGrid":true,"gridId":"63cf8cfe","isDynamic":true,"blockVersion":4,"sizing":{"width":"50%","widthMobile":"100%","widthTablet":""},"spacing":{"paddingBottomMobile":"24px"}} -->
+<!-- wp:generateblocks/image {"uniqueId":"57d85b20","mediaId":13501,"sizeSlug":"full","width":"100%","height":"100%","objectFit":"contain","blockVersion":2,"borders":{"borderTopLeftRadius":"8px","borderTopRightRadius":"8px","borderBottomLeftRadius":"8px","borderBottomRightRadius":"8px"}} -->
+<figure class="gb-block-image gb-block-image-57d85b20"><img class="gb-image gb-image-57d85b20"
+                                                            src="<?php echo placeholder_image_path( 1050, 700 ) ?>"
+                                                            alt=""
+                                                            title="dicaprio"/></figure>
+<!-- /wp:generateblocks/image -->
+<!-- /wp:generateblocks/container -->
+
+<!-- wp:generateblocks/container {"uniqueId":"dddbb436","isGrid":true,"gridId":"63cf8cfe","isDynamic":true,"blockVersion":4,"display":"flex","flexDirection":"column","justifyContent":"center","orderMobile":null,"sizing":{"width":"50%","widthMobile":"100%","height":"100%","widthTablet":""},"spacing":{"paddingLeft":"","paddingLeftMobile":"0px","paddingBottomMobile":"","paddingLeftTablet":""}} -->
+<!-- wp:generateblocks/headline {"uniqueId":"0461b8be","element":"p","blockVersion":3,"typography":{"fontSize":"48px","lineHeight":"60px","fontSizeTablet":"32px","lineHeightTablet":"40px"},"spacing":{"marginBottom":"24px"},"textColor":"#cf3942"} -->
+<p class="gb-headline gb-headline-0461b8be gb-headline-text">Lorem ipsum dolor sit amet, consectetur</p>
+<!-- /wp:generateblocks/headline -->
+
+<!-- wp:generateblocks/headline {"uniqueId":"d4252318","element":"p","blockVersion":3,"typography":{"fontSize":"20px","lineHeight":"28px","fontSizeTablet":"17px","lineHeightTablet":"24px"},"spacing":{"marginBottom":"24px"}} -->
+<p class="gb-headline gb-headline-d4252318 gb-headline-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+    exercitation ullamco laboris nisi ut</p>
+<!-- /wp:generateblocks/headline -->
+
+<!-- wp:generateblocks/button {"uniqueId":"2c15a32e","hasUrl":true,"target":true,"blockVersion":4,"display":"inline-flex","alignItems":"center","columnGap":"0.5em","typography":{"fontWeight":"500","fontSize":"20px","fontSizeTablet":"17px"},"spacing":{"paddingTop":"","paddingRight":"","paddingBottom":"","paddingLeft":""},"textColor":"#cf3942","hasIcon":true,"iconLocation":"right","iconPaddingRight":"","iconPaddingLeft":"0.5","iconStyles":{"height":"1em","width":"1em","widthTablet":"17px","heightTablet":"17px"}} -->
+<a class="gb-button gb-button-2c15a32e" href="https://saleziani.wip/ochrana-deti-a-zranitelnych-osob/kontakty"
+   target="_blank" rel="noopener noreferrer"><span class="gb-button-text">Čítať viac</span><span class="gb-icon"><svg
+                aria-hidden="true" role="img" height="1em" width="1em" viewBox="0 0 256 512"
+                xmlns="http://www.w3.org/2000/svg"><path fill="currentColor"
+                                                         d="M224.3 273l-136 136c-9.4 9.4-24.6 9.4-33.9 0l-22.6-22.6c-9.4-9.4-9.4-24.6 0-33.9l96.4-96.4-96.4-96.4c-9.4-9.4-9.4-24.6 0-33.9L54.3 103c9.4-9.4 24.6-9.4 33.9 0l136 136c9.5 9.4 9.5 24.6.1 34z"></path></svg></span></a>
+<!-- /wp:generateblocks/button -->
+<!-- /wp:generateblocks/container -->
+<!-- /wp:generateblocks/grid -->
+<!-- /wp:generateblocks/container -->
---
+++ src/legacy/patterns/lipt-caprio-image-text.php
@@ -0,0 +1,43 @@
+<?php
+
+declare( strict_types=1 );
+
+/**
+ * Title: LiptCaprio image text
+ * Slug: saleziani/lipt-caprio-image-text
+ * Categories: saleziani
+ */
+?>
+
+<!-- wp:generateblocks/container {"uniqueId":"c62f3c46","isDynamic":true,"blockVersion":4,"spacing":{"marginBottom":"8rem","marginBottomTablet":"4rem"},"metadata":{"categories":["saleziani"],"patternName":"saleziani/lipt-caprio-image-text","name":"LiptCaprio image text"}} -->
+<!-- wp:generateblocks/grid {"uniqueId":"23f28421","columns":2,"horizontalGap":30,"isDynamic":true,"blockVersion":3} -->
+<!-- wp:generateblocks/container {"uniqueId":"e9151243","isGrid":true,"gridId":"23f28421","isDynamic":true,"blockVersion":4,"sizing":{"width":"50%","widthMobile":"100%"},"spacing":{"paddingBottomMobile":"24px"}} -->
+<!-- wp:generateblocks/image {"uniqueId":"18463604","mediaId":13501,"sizeSlug":"full","width":"100%","height":"100%","objectFit":"contain","blockVersion":2,"borders":{"borderTopLeftRadius":"4px","borderTopRightRadius":"4px","borderBottomLeftRadius":"4px","borderBottomRightRadius":"4px"}} -->
+<figure class="gb-block-image gb-block-image-18463604"><img class="gb-image gb-image-18463604"
+                                                            src="<?php echo placeholder_image_path( 1050, 700 ) ?>"
+                                                            alt=""
+                                                            title="dicaprio"/></figure>
+<!-- /wp:generateblocks/image -->
+<!-- /wp:generateblocks/container -->
+
+<!-- wp:generateblocks/container {"uniqueId":"2a1d3efd","isGrid":true,"gridId":"23f28421","isDynamic":true,"blockVersion":4,"display":"flex","flexDirection":"column","justifyContent":"center","sizing":{"width":"50%","widthMobile":"100%","height":"100%"},"spacing":{"paddingRight":"","paddingLeft":"32px","paddingLeftTablet":"16px","paddingLeftMobile":"0px"}} -->
+<!-- wp:generateblocks/headline {"uniqueId":"4e0bdf67","element":"p","blockVersion":3,"typography":{"fontSize":"32px","lineHeight":"40px"},"spacing":{"marginBottom":"24px"},"textColor":"#cf3942"} -->
+<p class="gb-headline gb-headline-4e0bdf67 gb-headline-text">Lorem ipsum dolor sit amet, consectetur</p>
+<!-- /wp:generateblocks/headline -->
+
+<!-- wp:generateblocks/headline {"uniqueId":"7bfe24b6","element":"p","blockVersion":3,"typography":{"fontSize":"17px","lineHeight":"24px"},"spacing":{"marginBottom":"24px"}} -->
+<p class="gb-headline gb-headline-7bfe24b6 gb-headline-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+    exercitation ullamco laboris nisi ut</p>
+<!-- /wp:generateblocks/headline -->
+
+<!-- wp:generateblocks/button {"uniqueId":"16396de7","hasUrl":true,"target":true,"blockVersion":4,"display":"inline-flex","alignItems":"center","columnGap":"0.5em","typography":{"fontWeight":"500","fontSize":"17px","letterSpacing":""},"spacing":{"paddingTop":"","paddingRight":"","paddingBottom":"","paddingLeft":""},"textColor":"#cf3942","hasIcon":true,"iconLocation":"right","iconPaddingRight":"","iconPaddingLeft":"0.5","iconStyles":{"height":"1em","width":"1em"}} -->
+<a class="gb-button gb-button-16396de7" href="https://saleziani.wip/ochrana-deti-a-zranitelnych-osob/kontakty"
+   target="_blank" rel="noopener noreferrer"><span class="gb-button-text">Čítať viac</span><span class="gb-icon"><svg
+                aria-hidden="true" role="img" height="1em" width="1em" viewBox="0 0 256 512"
+                xmlns="http://www.w3.org/2000/svg"><path fill="currentColor"
+                                                         d="M224.3 273l-136 136c-9.4 9.4-24.6 9.4-33.9 0l-22.6-22.6c-9.4-9.4-9.4-24.6 0-33.9l96.4-96.4-96.4-96.4c-9.4-9.4-9.4-24.6 0-33.9L54.3 103c9.4-9.4 24.6-9.4 33.9 0l136 136c9.5 9.4 9.5 24.6.1 34z"></path></svg></span></a>
+<!-- /wp:generateblocks/button -->
+<!-- /wp:generateblocks/container -->
+<!-- /wp:generateblocks/grid -->
+<!-- /wp:generateblocks/container -->
