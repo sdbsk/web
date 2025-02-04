9d8db45ebba0f4e6df1c80f42de87f8d5bb658a1
“Filip <filip@bratia.sk>
2025-02-04 09:55:48 +0100

    Fix google material symbols installer

--- install-google-material-symbols.sh
+++ install-google-material-symbols.sh
@@ -15,16 +15,17 @@ echo "    $TEMP_DIR"
 rm -rf $TEMP_DIR
 mkdir -p $TEMP_DIR
 
-cd $TEMP_DIR && git clone --branch main --single-branch --depth 1 --filter=blob:none --no-checkout https://github.com/marella/material-symbols.git .  &>/dev/null
-cd $TEMP_DIR && git sparse-checkout init --cone  &>/dev/null
-cd $TEMP_DIR && git sparse-checkout set svg/300/outlined/  &>/dev/null
-cd $TEMP_DIR && git checkout  &>/dev/null
+cd $TEMP_DIR && git clone --branch main --single-branch --depth 1 --filter=blob:none --no-checkout https://github.com/marella/material-symbols.git .
+cd $TEMP_DIR && git sparse-checkout init --cone
+cd $TEMP_DIR && git sparse-checkout set svg/300/outlined/
+cd $TEMP_DIR && git checkout
 
 echo "  Moving icons to target directory:"
 echo "    $TARGET_DIR"
 rm -rf $TARGET_DIR
 mkdir -p $TARGET_DIR
-mv "${TEMP_DIR}svg/300/outlined/*" $TARGET_DIR
+
+mv ${TEMP_DIR}svg/300/outlined/*.svg $TARGET_DIR
 
 echo "  Building assets/blocks/scripts/icon-names.js"
 cd $CURRENT_DIR && node build-icons.js
